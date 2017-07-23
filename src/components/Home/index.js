3cf462cb5219a47fea516d8483172a2fb02e1fbb
sam yuyitung <samyuyitung@gmail.com>
2017-07-23 13:28:46 -0400

    Added title and start button button to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,12 +4,23 @@ import './Home.css';
 import Player from '../Player';
 
 class Home extends Component {
+  constructor(props) {
+        super(props);
+        this.startButtonPressed = this.startButtonPressed.bind(this);
+    }
+
   render() {
     return (
       <div className="Home">
-        <Player id="ue80QwXMRHg" />
+        <h1>MultiTube </h1>
+
+        <button onClick={this.startButtonPressed}> Click me!</button>
       </div>
     );
+  }
+
+  startButtonPressed() {
+    this.props.history.push('about')
   }
 }
 
