853227de1916de5a861707b30c6c52c13f48a791
sam yuyitung <samyuyitung@gmail.com>
2017-07-23 13:00:59 -0400

    Added react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from './routes';
+
+import './index.css';
+
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
